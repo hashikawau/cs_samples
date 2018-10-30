92a8308b63d326c9580faa07569d7e45360dfc24
hashikawa <hashikawa.yuuichi@gmail.com>
2018-10-30 21:58:04 +0900

    Added Logger class

---
+++ Common/Logger/Appenders.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace CSSamples.Common.Logger
+{
+    public class ConsoleAppender
+    {
+        public void Append(object sender, LogAppendingEventArgs args)
+        {
+            Console.Write($"[{args.Time}] [{args.Level,-5}] [{args.Tag}] ");
+            Console.WriteLine(args.Message);
+        }
+    }
+
+    public class DebugLogAppender
+    {
+        public void Append(object sender, LogAppendingEventArgs args)
+        {
+            Debug.Write($"[{args.Time}] [{args.Level,-5}] [{args.Tag}] ");
+            Debug.WriteLine(args.Message);
+        }
+    }
+
+    public class FileAppender
+    {
+        public string FilePath { get; private set; }
+
+        public FileAppender(string filePath)
+        {
+            FilePath = filePath;
+            if (Directory.Exists(FilePath))
+                throw new Exception(string.Format("file path is directory: FilePath={0}", FilePath));
+            string directory = Path.GetDirectoryName(FilePath);
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
+        }
+
+        public void Append(object sender, LogAppendingEventArgs args)
+        {
+            lock (FilePath)
+            {
+                using (var writer = new StreamWriter(
+                    new FileStream(FilePath, FileMode.Append, FileAccess.Write, FileShare.ReadWrite),
+                    Encoding.UTF8))
+                {
+                    writer.Write($"[{args.Time}] [{args.Level,-5}] [{args.Tag}] ");
+                    writer.WriteLine(args.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Common/Logger/Logger.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace CSSamples.Common.Logger
+{
+    public class Logger
+    {
+        public static LogLevel GlobalLevel { get; set; }
+#if DEBUG
+            = LogLevel.DEBUG;
+#else
+            = LogLevel.INFO;
+#endif
+
+        public static event EventHandler<LogAppendingEventArgs> Appenders;
+
+        public static Logger GetLogger(Type clazz)
+            => new Logger(clazz);
+
+        private string _tag;
+
+        public Logger(Type clazz)
+        {
+            _tag = clazz.Name;
+        }
+
+        private void WriteToAppenders(LogLevel level, string message)
+        {
+            if (level <= GlobalLevel)
+                Appenders?.Invoke(this, new LogAppendingEventArgs(level, _tag, message));
+        }
+
+        public void Error(Exception exception)
+            => WriteToAppenders(LogLevel.ERROR, string.Format("{0}\n{1}", exception.Message, exception.StackTrace));
+
+        public void Error(string format, params object[] args)
+            => WriteToAppenders(LogLevel.ERROR, string.Format(format, args));
+
+        public void Warn(string format, params object[] args)
+            => WriteToAppenders(LogLevel.WARN, string.Format(format, args));
+
+        public void Info(string format, params object[] args)
+            => WriteToAppenders(LogLevel.INFO, string.Format(format, args));
+
+        public void Debug(string format, params object[] args)
+            => WriteToAppenders(LogLevel.DEBUG, string.Format(format, args));
+    }
+
+    public enum LogLevel
+    {
+        ERROR,
+        WARN,
+        INFO,
+        DEBUG
+    }
+
+    public class LogAppendingEventArgs
+    {
+        public string Time { get; private set; }
+        public LogLevel Level { get; private set; }
+        public string Tag { get; private set; }
+        public string Message { get; private set; }
+
+        public LogAppendingEventArgs(LogLevel level, string tag, string message)
+        {
+            Time = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff");
+            Level = level;
+            Tag = tag;
+            Message = message;
+        }
+    }
+}
